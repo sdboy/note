4e6d5ec4b1294f1a2752be59f7faca57bdbaf669
jiaoguang <jiaoyiguang@gmail.com>
2019-03-20 10:52:58 +0800

    划分模块

---
+++ note-bean/src/main/java/com/sdboy/note/bean/InitBean.java
@@ -0,0 +1,12 @@
+package com.sdboy.note.bean;
+
+/**
+ * <p></p>
+ *
+ * @author JG
+ * @version V1.0.0
+ * @date 2019/3/20 10:48
+ * @see
+ */
+public class InitBean {
+}
---
+++ note-common/src/main/java/com/sdboy/note/common/InitCommon.java
@@ -0,0 +1,12 @@
+package com.sdboy.note.common;
+
+/**
+ * <p></p>
+ *
+ * @author JG
+ * @version V1.0.0
+ * @date 2019/3/20 10:49
+ * @see
+ */
+public class InitCommon {
+}
