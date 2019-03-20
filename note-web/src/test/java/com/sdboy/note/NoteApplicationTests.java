a5b012bc72a2a8a0cad53b3eeca20d22e40132db
jiaoguang <jiaoyiguang@gmail.com>
2019-03-20 10:57:11 +0800

    增加测试

---
+++ note-web/src/test/java/com/sdboy/note/NoteApplicationTests.java
@@ -0,0 +1,22 @@
+package com.sdboy.note;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * <p></p>
+ *
+ * @author JG
+ * @version V1.0.0
+ * @date 2019/3/20 10:55
+ * @see
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NoteApplicationTests {
+  @Test
+  public void contextLoads() {
+  }
+}
